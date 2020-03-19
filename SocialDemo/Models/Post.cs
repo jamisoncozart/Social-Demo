9b8f37eb73db1f7c909e6db47b2513927cdec7bc
Jamison Cozart <jamisoncozart@gmail.com>
2020-03-19 07:49:50 -0700

    Update Post class

--- SocialDemo/Models/Post.cs
+++ SocialDemo/Models/Post.cs
@@ -6,6 +6,7 @@ namespace SocialDemo.Models
   {
     public string Title { get; set; }
     public string Body { get; set; }
+    public string Author { get; set; }
     public int Upvotes { get; set; }
     public virtual ICollection<Comment> Comments { get; set; }
     public int PostId { get; set; }
